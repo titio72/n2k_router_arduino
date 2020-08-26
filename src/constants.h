ed04a95102b4763d6c0c86ea75b6f849b5d731a2
aboni <andrboni@gmail.com>
2020-08-26 07:33:24 +0200

    missing constants

--- src/constants.h
+++ src/constants.h
@@ -1,18 +1,22 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define RXD2 16
-#define TXD2 17
-#define SSID "yourssid"
-#define PSWD "yourpswd"
-#define OK 0
-#define KO -1
+#define RXD2 12
+#define TXD2 14
+#define MY_SSID "myssid"
+#define MY_PSWD "mypwd"
 #define UDP_DEST "192.168.2.99"
 #define UDP_PORT 1222
 
-#define TRACE_DHT11   0
-#define TRACE_BMP280  0
+#define TRACE_DHT11   1
+#define TRACE_BMP280  1
 #define TRACE_UDP     0
 #define TRACE_CAN     0
+
+#define I2C_SDA 22
+#define I2C_SCL 21
+#define SEALEVELPRESSURE_HPA (1013.25)
+#define DHTPIN 4
+#define WIFI 0
 
 #endif
